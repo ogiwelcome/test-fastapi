f90bb8f0e87893f90a9e8070794de1ef55801c42
ogiwelcome <ogi.fordevelopment@gmail.com>
2022-08-13 21:55:08 +0900

    hoge

--- server/app/main.py
+++ server/app/main.py
@@ -1,5 +1,7 @@
 from typing import Union
+import os
 
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -8,3 +10,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=os.getenv("PORT"))
